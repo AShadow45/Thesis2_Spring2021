0896a65193f936f0dbb39e1dade83682094d09c5
Analisse Guadeloupe <31444172+AShadow45@users.noreply.github.com>
2021-02-08 20:30:54 -0500

    Movement Script Updates

--- Thesis_Project_2021/Assets/Movement_Player.cs
+++ Thesis_Project_2021/Assets/Movement_Player.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Movement_Player : MonoBehaviour
 {
-    
-    
+    public float speed = 5f;
+    Rigidbody2D rb;
+
+    r
+
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        // KEYBOARD CONTROLS
+
+        float xSpeed = Input.GetAxis("Horizontal") * speed;
+        float ySpeed = Input.GetAxis("Vertical") * speed;
+        rb.velocity = new Vector2(xSpeed, ySpeed);
     }
 }
